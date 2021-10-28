b30f975b1a37f8f943371d03e26b3381dc1a4db1
Alexis <alexis.auriac@kbrwadventure.com>
2021-10-28 13:32:17 +0200

    alexis_fantom: disabled logging, made script executable

--- alexis_fantom.py
+++ alexis_fantom.py
@@ -21,21 +21,21 @@ port = 12000
 """
 set up fantom logging
 """
-fantom_logger = logging.getLogger()
-fantom_logger.setLevel(logging.DEBUG)
-formatter = logging.Formatter(
-    "%(asctime)s :: %(levelname)s :: %(message)s", "%H:%M:%S")
-# file
-if os.path.exists("./logs/fantom.log"):
-    os.remove("./logs/fantom.log")
-file_handler = RotatingFileHandler('./logs/fantom.log', 'a', 1000000, 1)
-file_handler.setLevel(logging.DEBUG)
-file_handler.setFormatter(formatter)
-fantom_logger.addHandler(file_handler)
-# stream
-stream_handler = logging.StreamHandler()
-stream_handler.setLevel(logging.WARNING)
-fantom_logger.addHandler(stream_handler)
+# fantom_logger = logging.getLogger()
+# fantom_logger.setLevel(logging.DEBUG)
+# formatter = logging.Formatter(
+#     "%(asctime)s :: %(levelname)s :: %(message)s", "%H:%M:%S")
+# # file
+# if os.path.exists("./logs/fantom.log"):
+#     os.remove("./logs/fantom.log")
+# file_handler = RotatingFileHandler('./logs/fantom.log', 'a', 1000000, 1)
+# file_handler.setLevel(logging.DEBUG)
+# file_handler.setFormatter(formatter)
+# fantom_logger.addHandler(file_handler)
+# # stream
+# stream_handler = logging.StreamHandler()
+# stream_handler.setLevel(logging.WARNING)
+# fantom_logger.addHandler(stream_handler)
 
 
 class Player():
