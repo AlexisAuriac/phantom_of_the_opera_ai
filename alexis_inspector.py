a6d21270878cc1ba6cfcff5682925dda0fda25d7
Alexis <alexis.auriac@kbrwadventure.com>
2021-10-28 13:32:58 +0200

    alexis_inspector: added experimental algo to play twice when possible
    Also:
    - Disbaled logging
    - Made script executable

--- alexis_inspector.py
+++ alexis_inspector.py
@@ -3,6 +3,7 @@
 import json
 import logging
 import os
+import copy
 import random
 import socket
 from logging.handlers import RotatingFileHandler
@@ -21,21 +22,68 @@ port = 12000
 """
 set up inspector logging
 """
-inspector_logger = logging.getLogger()
-inspector_logger.setLevel(logging.DEBUG)
-formatter = logging.Formatter(
-    "%(asctime)s :: %(levelname)s :: %(message)s", "%H:%M:%S")
-# file
-if os.path.exists("./logs/inspector.log"):
-    os.remove("./logs/inspector.log")
-file_handler = RotatingFileHandler('./logs/inspector.log', 'a', 1000000, 1)
-file_handler.setLevel(logging.DEBUG)
-file_handler.setFormatter(formatter)
-inspector_logger.addHandler(file_handler)
-# stream
-stream_handler = logging.StreamHandler()
-stream_handler.setLevel(logging.WARNING)
-inspector_logger.addHandler(stream_handler)
+# inspector_logger = logging.getLogger()
+# inspector_logger.setLevel(logging.DEBUG)
+# formatter = logging.Formatter(
+#     "%(asctime)s :: %(levelname)s :: %(message)s", "%H:%M:%S")
+# # file
+# if os.path.exists("./logs/inspector.log"):
+#     os.remove("./logs/inspector.log")
+# file_handler = RotatingFileHandler('./logs/inspector.log', 'a', 1000000, 1)
+# file_handler.setLevel(logging.DEBUG)
+# file_handler.setFormatter(formatter)
+# inspector_logger.addHandler(file_handler)
+# # stream
+# stream_handler = logging.StreamHandler()
+# stream_handler.setLevel(logging.WARNING)
+# inspector_logger.addHandler(stream_handler)
+
+def get_best_play(game_state, plays):
+    best_play = None
+    min_carlotta_move = None
+    best_game_state = None
+
+    for play in plays:
+        new_game_state = immutable_play(game_state, play)
+        (scream, no_scream) = predict_carlotta_move_inspector(new_game_state)
+        new_min_carlotta_move = abs(scream - no_scream)
+
+        if best_play == None or new_min_carlotta_move < min_carlotta_move:
+            best_play = play
+            min_carlotta_move = new_min_carlotta_move
+            best_game_state = new_game_state
+
+    return (best_play, min_carlotta_move, best_game_state)
+
+
+def immutable_play_rec2(question):
+    plays = get_all_possible_plays(question)
+
+    best_play = None
+    min_carlotta_move = None
+    for play in plays:
+        new_game_state = immutable_play(question["game state"], play)
+
+        new_data = copy.deepcopy(question["data"])
+        new_data.pop(play[0])
+        new_question = {
+            "question type": question["question type"],
+            "data": new_data,
+            "game state": new_game_state
+        }
+
+        plays2 = get_all_possible_plays(new_question)
+
+        for play2 in plays2:
+            new_game_state2 = immutable_play(new_game_state, play2)
+            (scream, no_scream) = predict_carlotta_move_inspector(new_game_state2)
+            new_min_carlotta_move = abs(scream - no_scream)
+
+            if best_play == None or new_min_carlotta_move < min_carlotta_move:
+                best_play = play + play2
+                min_carlotta_move = new_min_carlotta_move
+
+    return best_play
 
 
 class Player():
@@ -47,6 +95,7 @@ class Player():
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.response_stack = []
+        self.play = []
 
     def connect(self):
         self.socket.connect((host, port))
@@ -56,21 +105,14 @@ class Player():
 
     def answer(self, question):
         if self.response_stack == []:
-            plays = get_all_possible_plays(question)
+            if (question["game state"]["num_tour"] - 1) % 2 == 1:
+                self.response_stack = immutable_play_rec2(question)
+            else:
+                plays = get_all_possible_plays(question)
+                (best_play, _, _) = get_best_play(question["game state"], plays)
 
-            best_play = None
-            min_carlotta_move = None
-            for play in plays:
-                new_game_state = immutable_play(question["game state"], play)
-                # new_min_carlotta_move = predict_carlotta_move_inspector(new_game_state)
-                (scream, no_scream) = predict_carlotta_move_inspector(new_game_state)
-                new_min_carlotta_move = abs(scream - no_scream)
+                self.response_stack = best_play
 
-                if best_play == None or new_min_carlotta_move < min_carlotta_move:
-                    best_play = play
-                    min_carlotta_move = new_min_carlotta_move
-
-            self.response_stack = best_play
         if question["question type"] == "select character":
             return self.response_stack.pop(0)
         else:
